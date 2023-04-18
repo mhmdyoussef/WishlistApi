3175a04837c6a67e545df99b0a2e13ac98669436
Mohamed Youssef <egsyntax@gmail.com>
2023-04-18 10:41:09 +0200

    compatible version added

--- registration.php
+++ registration.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * @author MYDev 2023.
+ * @version 1.0.0
+ * @link (https://my-dev.pro)
+ */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
